8167390295ad80a52fac908192f37685510c55e3
hafiz-aa <mhafiz.aa@gmail.com>
2023-07-14 16:46:46 +0700

    Tipe Data Union

---
+++ dist/tests/union.test.js
@@ -0,0 +1,28 @@
+"use strict";
+describe("Union Type", () => {
+    it("Should support union in typescript", function () {
+        let sample = "Hafiz";
+        console.info(sample);
+        sample = 100;
+        console.info(sample);
+        sample = true;
+        console.info(sample);
+        //sample = [];
+    });
+    it("Should process the union type", () => {
+        function process(value) {
+            if (typeof value === "string") {
+                return value.toUpperCase();
+            }
+            else if (typeof value === "number") {
+                return value + 2;
+            }
+            else {
+                return !value;
+            }
+        }
+        expect(process(100)).toBe(102);
+        expect(process("Hafiz")).toBe("HAFIZ");
+        expect(process(true)).toBe(false);
+    });
+});
