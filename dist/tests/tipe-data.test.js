7f6507631e27e3fb766e57b6076c9fcbee07458c
hafiz-aa <mhafiz.aa@gmail.com>
2023-07-14 15:03:38 +0700

    Tipe Data

---
+++ dist/tests/tipe-data.test.js
@@ -0,0 +1,11 @@
+"use strict";
+describe("Data Type", () => {
+    it("Should must declare", function () {
+        const name = "Muhammad Hafiz";
+        const balance = 100000;
+        const isVip = true;
+        console.log(name);
+        console.log(balance);
+        console.log(isVip);
+    });
+});
---
+++ tests/tipe-data.test.ts
@@ -0,0 +1,11 @@
+describe("Data Type", () => {
+  it("Should must declare", function () {
+    const name: string = "Muhammad Hafiz";
+    const balance: number = 100000;
+    const isVip: boolean = true;
+
+    console.log(name);
+    console.log(balance);
+    console.log(isVip);
+  });
+});
