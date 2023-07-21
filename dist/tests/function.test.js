050d38d616891a1f1bfd468ec9ca5830c35d7629
hafiz-aa <mhafiz.aa@gmail.com>
2023-07-21 18:02:18 +0700

    Function Parameter

--- dist/tests/function.test.js
+++ dist/tests/function.test.js
@@ -10,4 +10,33 @@ describe("Function", function () {
         }
         printHello("Hafiz");
     });
+    it("Should support default value", function () {
+        function sayHello(name = "Guest") {
+            return `Hello ${name}`;
+        }
+        expect(sayHello()).toBe("Hello Guest");
+        expect(sayHello("Hafiz")).toBe("Hello Hafiz");
+    });
+    it("Should support rest parameter", function () {
+        function sum(...values) {
+            let total = 0;
+            for (const value of values) {
+                total += value;
+            }
+            return total;
+        }
+        expect(sum(1, 2, 3, 4, 5)).toBe(15);
+    });
+    it("Should support optional parameter", function () {
+        function sayHello(firstName, lastName) {
+            if (lastName) {
+                return `Hello ${firstName} ${lastName}`;
+            }
+            else {
+                return `Hello ${firstName}`;
+            }
+        }
+        expect(sayHello("Hafiz")).toBe("Hello Hafiz");
+        expect(sayHello("Hafiz", "Aziz")).toBe("Hello Hafiz Aziz");
+    });
 });
--- tests/function.test.ts
+++ tests/function.test.ts
@@ -12,4 +12,35 @@ describe("Function", function () {
 
     printHello("Hafiz");
   });
+
+  it("Should support default value", function () {
+    function sayHello(name: string = "Guest"): string {
+      return `Hello ${name}`;
+    }
+    expect(sayHello()).toBe("Hello Guest");
+    expect(sayHello("Hafiz")).toBe("Hello Hafiz");
+  });
+
+  it("Should support rest parameter", function () {
+    function sum(...values: number[]): number {
+      let total = 0;
+      for (const value of values) {
+        total += value;
+      }
+      return total;
+    }
+    expect(sum(1, 2, 3, 4, 5)).toBe(15);
+  });
+
+  it("Should support optional parameter", function () {
+    function sayHello(firstName: string, lastName?: string): string {
+      if (lastName) {
+        return `Hello ${firstName} ${lastName}`;
+      } else {
+        return `Hello ${firstName}`;
+      }
+    }
+    expect(sayHello("Hafiz")).toBe("Hello Hafiz");
+    expect(sayHello("Hafiz", "Aziz")).toBe("Hello Hafiz Aziz");
+  });
 });
