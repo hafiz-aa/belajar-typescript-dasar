cb93853e42c0c2e9f0a85a0cba60a86f640e0aed
hafiz-aa <mhafiz.aa@gmail.com>
2023-07-26 16:01:48 +0700

    Break & Continue

--- tests/loop.test.ts
+++ tests/loop.test.ts
@@ -31,4 +31,21 @@ describe("Loop", () => {
       counter++;
     } while (counter < 10);
   });
+
+  it("Should support break and continue", function () {
+    let counter: number = 0;
+
+    do {
+      counter++;
+
+      if (counter == 10) {
+        break;
+      }
+
+      if (counter % 2 == 0) {
+        continue;
+      }
+      console.info(counter);
+    } while (true);
+  });
 });
